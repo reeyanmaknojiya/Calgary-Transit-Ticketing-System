4526425d82edef1a21ac8b2c49ffcd886fb08df0
rileyPupp <rpupp99@gmail.com>
2021-02-26 15:50:44 -0700

    Add project files.

---
+++ TransitHub/Controllers/Login.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransitHub.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string emailInput, string passwordInput) => RedirectToAction("Index", "Home");
+    }
+}
