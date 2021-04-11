9bf525427684861add058cc055796575ab64f706
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-11 16:43:13 -0600

    Updated login and signup

--- TransitHubXAML/loginPage.xaml.cs
+++ TransitHubXAML/loginPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,19 +27,53 @@ namespace TransitHubXAML
         }
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            if (EmailInput.Text == "johnson@gmail.com" & PasswordInput.Text == "johnson123")
+            string email = emailText.Text;
+            string password = passwordText.Text;
+            bool valid = false;
+
+            //Pass the filepath and filename to the StreamWriter Constructor
+            string _filePath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            _filePath = Directory.GetParent(Directory.GetParent(_filePath).FullName).FullName;
+            _filePath += @"\Accounts.txt";
+
+            StreamReader sr = new StreamReader(_filePath);
+
+            string line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
             {
-                User.loggedIn = true;
-                User.firstName = "John";
-                User.lastName = "Johnson";
-                User.bMonth = "March";
-                User.bDay = "1";
-                User.bYear = "1989";
-                User.email = "johnjohnson@gmail.com";
-                User.password = "johnson123";
-                User.phone = "(403)123-4567";
+                if (line.Equals(email))
+                {
+                    string passwordString = sr.ReadLine();
+                    if (passwordString.Equals(password))
+                    {
+                        valid = true;
+                        string fname = sr.ReadLine();
+                        string lname = sr.ReadLine();
+                        string bmonth = sr.ReadLine();
+                        string bday = sr.ReadLine();
+                        string byear = sr.ReadLine();
+                        string phoneN = sr.ReadLine();
+
+                        User.firstName = fname;
+                        User.lastName = lname;
+                        User.bMonth = bmonth;
+                        User.bDay = bday;
+                        User.bYear = byear;
+                        User.email = email;
+                        User.phone = phoneN;
+                        User.password = password;
+                        User.loggedIn = true;
+                    }
+                }
+                //account.Add(line);
+                line = sr.ReadLine();
             }
-            this.NavigationService.Navigate(new storePage());
+            sr.Close();
+            if (valid == true)
+            {
+                this.NavigationService.Navigate(new storePage());
+            }
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
