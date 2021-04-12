b1249bd02f38d3ecd3e78d099dc040a11c18a4be
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-12 17:30:06 -0600

    input validation

--- TransitHubXAML/EditAccountNamePage.xaml.cs
+++ TransitHubXAML/EditAccountNamePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +20,7 @@ namespace TransitHubXAML
     /// </summary>
     public partial class EditAccountNamePage : Page
     {
+        bool valid = true;
         public EditAccountNamePage()
         {
             InitializeComponent();
@@ -27,6 +28,38 @@ namespace TransitHubXAML
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new AccountEditPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AccountEditPage());
+        }
+
+        // Confirm Button
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            firstNameText.BorderBrush = System.Windows.Media.Brushes.Gray;
+            lastNameText.BorderBrush = System.Windows.Media.Brushes.Gray;
+
+            valid = true;
+
+            if (String.IsNullOrEmpty(firstNameText.Text))
+            {
+                //nameError.Visibility = Visibility.Visible;
+                firstNameText.BorderBrush = System.Windows.Media.Brushes.Red;
+                valid = false;
+            }
+            if (String.IsNullOrEmpty(lastNameText.Text))
+            {
+                lastNameText.BorderBrush = System.Windows.Media.Brushes.Red;
+                valid = false;
+            }
+            if (valid == true)
+            {
+                User.firstName = firstNameText.Text;
+                User.lastName = lastNameText.Text;
+                this.NavigationService.Navigate(new AccountEditPage());
+            }
         }
     }
 }
