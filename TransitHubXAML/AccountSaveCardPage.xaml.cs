c158c173791663abc495686cd2ceb80f307ba181
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-12 18:43:17 -0600

    card input validation

--- TransitHubXAML/AccountSaveCardPage.xaml.cs
+++ TransitHubXAML/AccountSaveCardPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace TransitHubXAML
 {
@@ -27,6 +28,92 @@ namespace TransitHubXAML
         private void Back_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new AccountPaymentMethodsPage());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: Verify input
+            bool badInput = false;
+            if (!Regex.IsMatch(cardHolder.Text, "^(\\s)*[A-Za-z]+((\\s)?((\\'|\\-|\\.)?([A-Za-z])+))*(\\s)*$"))
+            {
+                //Bad name
+                cardHolder.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                cardholderLabel.Content = "Cardholder Name:*";
+                badInput = true;
+            }
+            else
+            {
+                cardHolder.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                cardholderLabel.Content = "Cardholder Name:";
+            }
+
+            if (!Regex.IsMatch(cardNumber.Text, "\\b\\d{4}[ -]?\\d{4}[ -]?\\d{4}[ -]?\\d{4}\\b"))
+            {
+                //Bad credit card
+                cardNumber.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                cardNumberLabel.Content = "Card Number:*";
+                badInput = true;
+            }
+            else
+            {
+                cardNumber.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                cardNumberLabel.Content = "Card Number:";
+            }
+
+            if (monthBox.SelectedIndex == -1)
+            {
+                monthBox.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                dateLabel.Content = "Date:*";
+                badInput = true;
+            }
+            else
+            {
+                monthBox.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                dateLabel.Content = "Date:";
+            }
+
+            if (yearBox.SelectedIndex == -1)
+            {
+                yearBox.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                dateLabel.Content = "Date:*";
+                badInput = true;
+            }
+            else
+            {
+                yearBox.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                dateLabel.Content = "Date:";
+            }
+
+            if (!Regex.IsMatch(cvvIn.Text, "\\d{3}"))
+            {
+                //bad cvv
+                cvvIn.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                cvvLabel.Content = "CVV:*";
+                badInput = true;
+            }
+            else
+            {
+                cvvIn.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                cvvLabel.Content = "CVV:";
+            }
+
+            if (!Regex.IsMatch(zipIn.Text, "([A-Z]\\d){3}") && !Regex.IsMatch(zipIn.Text, "^\\d{5}(?:[-\\s]\\d{4})?$"))
+            {
+                //bad postal code
+                zipIn.Background = new SolidColorBrush(System.Windows.Media.Colors.LightSalmon);
+                zipLabel.Content = "ZIP Code:*";
+                badInput = true;
+            }
+            else
+            {
+                zipIn.Background = new SolidColorBrush(System.Windows.Media.Colors.White);
+                zipLabel.Content = "ZIP Code:";
+            }
+
+            if (!badInput)
+            {
+                this.NavigationService.Navigate(new CardConfirmationPage());
+            }
         }
     }
 }
