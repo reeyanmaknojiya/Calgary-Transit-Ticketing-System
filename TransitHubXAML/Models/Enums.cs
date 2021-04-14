aad80b99e45838ca5d5b0483f72623f475542541
reeyanmaknojiya <47335900+reeyanmaknojiya@users.noreply.github.com>
2021-04-14 05:39:48 -0600

    Consistency

---
+++ TransitHubXAML/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitHubXAML.Models
+{
+    public enum Enums
+    {
+        adultTicket,
+        youthTicket,
+        adultMonthly,
+        youthMonthly,
+        lowIncomeMonthly
+    }
+}
---
+++ TransitHubXAML/Models/paypal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitHubXAML.Models
+{
+    public class paypal
+    {
+        string email { get; set; }
+
+        string password { get; set; }
+    }
+}
